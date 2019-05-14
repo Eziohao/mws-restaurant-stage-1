15b133c5019db64afb1321d7832d55147d54be0c
Wenhao Zhang <zhangw@opentext.com>
2019-05-14 15:07:06 -0400

    update restaurant info page css

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -118,18 +118,22 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.className="name"
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
+  date.className="date"
   date.innerHTML = review.date;
   li.appendChild(date);
 
   const rating = document.createElement('p');
-  rating.innerHTML = `Rating: ${review.rating}`;
+  rating.className="rating"
+  rating.innerHTML = `RATING: ${review.rating}`;
   li.appendChild(rating);
 
   const comments = document.createElement('p');
+  comments.className="comment"
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
